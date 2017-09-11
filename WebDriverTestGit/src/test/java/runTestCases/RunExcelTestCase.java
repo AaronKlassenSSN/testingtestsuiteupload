f541147b37c9de1af6e2e8ffe54127d542d0f57d
AaronKlassenSSN <aaron.klassen@screenscape.com>
2017-09-11 13:08:32 -0300

    monday commit

--- WebDriverTestGit/src/test/java/runTestCases/RunExcelTestCase.java
+++ WebDriverTestGit/src/test/java/runTestCases/RunExcelTestCase.java
@@ -2,10 +2,8 @@ package runTestCases;
 
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
